846118149faf173d8d9f26136a9d179df2e1e072
naomiajacobs <naomiajacobs@gmail.com>
2015-08-28 14:56:31 -0700

    Draw enemies in svg element

--- client/watchout.js
+++ client/watchout.js
@@ -1,11 +1,31 @@
 var gameBoardData = [{
   background: "black",
-  height: '500px',
-  width: '500px'
+  height: '700px',
+  width: '700px'
 }];
 
-d3.select("body").selectAll("div").data(gameBoardData, function(d) {return d})
-  .enter().append("div")
+var numEnemies = 30;
+
+var enemyData = _.map(_.range(0, numEnemies), function(item, key) {
+  return {
+    color: "blue",
+    id: key,
+    x: Math.random() * 700,
+    y: Math.random() * 700,
+    radius: 10
+  }
+});
+
+var board = d3.select("body").selectAll("svg").data(gameBoardData, function(d) {return d})
+  .enter().append("svg")
     .style("width", function(d) {return d.width})
     .style("height", function(d) {return d.height})
-    .style("background-color", function(d) {return d.background});
+    .style("background-color", function(d) {return d.background})
+
+var enemies = d3.select("svg").selectAll("circle")
+  .data(enemyData)
+  .enter().append("circle")
+    .attr("cx", function(d) {return d.x})
+    .attr("cy", function(d) {return d.y})
+    .attr("r", function(d) {return d.radius})
+    .style("fill", function(d) {return d.color});
