afd449687e33fbbfe48ee0a22b63c6ef25a4e1d4
naomiajacobs <naomiajacobs@gmail.com>
2015-08-28 14:34:45 -0700

    Create gameboard

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,11 @@
-// start slingin' some d3 here.
+var gameBoardData = [{
+  background: "black",
+  height: '500px',
+  width: '500px'
+}];
 
+d3.select("body").selectAll("div").data(gameBoardData, function(d) {return d})
+  .enter().append("div")
+    .style("width", function(d) {return d.width})
+    .style("height", function(d) {return d.height})
+    .style("background-color", function(d) {return d.background});
