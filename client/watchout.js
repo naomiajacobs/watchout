dd9cb841fc7f87f61e3c094fbb5c49417de1a27f
naomiajacobs <naomiajacobs@gmail.com>
2015-08-29 11:46:19 -0700

    Makes shuriken spin

--- client/watchout.js
+++ client/watchout.js
@@ -3,7 +3,7 @@ var highScore = 0;
 var collisions = 0;
 
 var gameBoardData = [{
-  background: "MidnightBlue",
+  background: "LightBlue",
   height: '700px',
   width: '700px'
 }];
@@ -56,11 +56,27 @@ var enemyData = _.map(_.range(0, numEnemies), function(item, key) {
   return {
     color: colors[Math.floor(Math.random() * colors.length)],
     id: key,
-    x: function() { return Math.random() * 700 },
-    y: function() { return Math.random() * 700 },
+    x: undefined,
+    y: undefined,
+    setNewX: function() {
+      this.x = Math.random() * 700;
+      return this.x
+    },
+    setNewY: function() {
+      this.y = Math.random() * 700;
+      return this.y;
+    },
     width: 30,
-    height: 30
-  }
+    height: 30,
+    // fullspin:
+    //   '@-webkit-keyframes fullspin {
+    //     from {-webkit-transform:rotate(0deg);}
+    //     to {-webkit-transform:rotate(360deg);}
+    //   }',
+    // animationName: fullspin,
+    // animationDuration: '3s',
+    // animationIterationCount: infinite
+  };
 });
 
 var playerData = [
@@ -85,25 +101,17 @@ var board = d3.select("body").selectAll("svg").data(gameBoardData, function(d) {
     .style("height", function(d) {return d.height})
     .style("background-color", function(d) {return d.background})
 
-// var enemies = d3.select("svg").selectAll("circle")
-//   .data(enemyData)
-//   .enter().append("circle")
-//     .attr("cx", function(d) {return d.x()})
-//     .attr("cy", function(d) {return d.y()})
-//     .attr("r", function(d) {return d.radius})
-//     .style("fill", function(d) {return d.color})
-//     .classed("circle", true);
-
 var enemies = d3.select("svg").selectAll("image")
   .data(enemyData)
   .enter().append("image")
-    .attr("x", function(d) {return d.x()})
-    .attr("y", function(d) {return d.y()})
+    .attr("x", function(d) {return d.setNewX()})
+    .attr("y", function(d) {return d.setNewY()})
     .attr("width", function(d) {return d.width})
     .attr("height", function(d) {return d.height})
     .attr("xlink:href", "shuriken.svg")
     .style("fill", function(d) {return d.color})
-    .classed("circle", true);
+    .classed("circle", true)
+    .attr("transform-origin", "50% 50%");
 
 var player = d3.select("svg").selectAll("ellipse")
   .data(playerData)
@@ -121,19 +129,12 @@ var moveEnemies = function() {
     .transition()
       .ease("elastic")
       .duration(5000)
-      .attr("x", function(d) {return d.x()})
-      .attr("y", function(d) {return d.y()})
+      .attr("x", function(d) {return d.setNewX()})
+      .attr("y", function(d) {return d.setNewY()})
 };
 
-moveEnemies();
 
-// var findDistance = function(enemy) {
-//   var enemyX = enemy.getAttribute("cx");
-//   var enemyY = enemy.getAttribute("cy");
-//   var playerX = document.getElementsByClassName("player")[0].getAttribute("cx");
-//   var playerY = document.getElementsByClassName("player")[0].getAttribute("cy");
-//   return Math.sqrt(Math.pow((playerX - enemyX), 2) + Math.pow((playerY - enemyY), 2));
-// };
+moveEnemies();
 
 var findDistance = function(enemy) {
   var enemyX = enemy.getAttribute("x");
@@ -149,7 +150,6 @@ var detectCollision = function(enemy) {
     return true;
   }
 };
-
 
 var detectCollisions = function() {
   _.each(document.getElementsByClassName("circle"), function(enemy) { //enemy should be an object with an x prop
